d831b16398536a8273a37c315546d3eee4d52c74
Clarence Chan <admin@Clarences-MacBook-Pro.local>
2018-07-08 12:17:59 +0800

    Updated footer, updated url in tweet message to include correct link

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     $("#getQuote").on("click", function () {
         $.getJSON(
-            "https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520", //JSON file from https://github.com/dmakk767
+            "https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520",
             function (json) {
                 randomQuoteIndex = Math.floor(Math.random() * (json.length + 1));
                 obj = Object.values(json[randomQuoteIndex]);
@@ -19,7 +19,7 @@ $(document).ready(function () {
 
     $("#tweet").on("click", function () {
         window.open(
-            "https://twitter.com/intent/tweet?hashtags=quotes&text=\"" + obj[0] + "\" - " + obj[1] + "&url=https%3A%2F%2Fclarencecodes.github.io%2Frandom-quote-viewer"
+            "https://twitter.com/intent/tweet?hashtags=quotes&text=\"" + obj[0] + "\" - " + obj[1] + "&url=https%3A%2F%2Fclarencecodes.github.io%2Frandom-quote-machine%2Findex.html"
         );
     });
 });
